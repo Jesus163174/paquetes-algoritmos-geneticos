905919d1fa02bd14458a2855665fb1cb113e533a
Jesus163174 <163174@ids.upchiapas.edu.mx>
2019-07-12 11:54:50 -0500

    add

--- paquetes.py
+++ paquetes.py
@@ -9,8 +9,10 @@ M    = 20  # TAMAÑO DEL BUFFER
 TMAX = 8  # NUMERO DE INDIVIDUOS, POBLACION MAXIMA
 T0   = 2   # POBLACIÓN INICIAL
 CDPO = 50  # % PARA DETENER EL CICLO
+CRUZA = 1
 
-
+PORCENTA = int(( TMAX * CDPO ) / 100)
+print(PORCENTA)
 paquetes = dict()
 maximos = []
 minimos = []
@@ -29,14 +31,15 @@ for i in range(T0):
     indices    = list(paquetes.keys()) # generar un arreglo con el tamaño K
     random.shuffle(indices)  # hacer un random con las posiciones
     poblacion.append(indices) # generar la población
-parar = 0
-while parar < 2:
+PARAR = False
+parar = 0 
+while PARAR != True :
 #cruza
     for i in range(T0):
         for j in range(i+1,T0):
+              
             poblacion.append(poblacion[i][0:int(K/2)]+poblacion[j][int(K/2):K])
             poblacion.append(poblacion[j][0:int(K/2)]+poblacion[i][int(K/2):K])
-
     i = T0
     iAux = T0+1
     valores = []
@@ -47,6 +50,7 @@ while parar < 2:
                 valores.append(poblacion[i][j])
                 jAux = 0
                 while jAux < K:
+                    print("here aqui")
                     if poblacion[iAux].count(poblacion[iAux][jAux]) > 1:
                         valores.append(poblacion[iAux][jAux])
                         poblacion[i][j] = valores[1]
@@ -59,6 +63,8 @@ while parar < 2:
             i+=1
         if iAux+1 < len(poblacion):
             iAux+=1
+        print("here")
+    
 
     pos = []
     valores = []
@@ -90,18 +96,48 @@ while parar < 2:
 
     poblacion.sort(key=lambda poblacion: poblacion[K+1])
 
+    for i in range(len(poblacion)):
+        print(poblacion[i])
+
     mini = min(poblacion, key=lambda poblacion: poblacion[K+1])
     maxi = max(poblacion, key=lambda poblacion: poblacion[K+1])
     promedio = 0
     for i in range(len(poblacion)):
         promedio += poblacion[i][K+1]
     promedio /=len(poblacion)
+
+    promedios.append(promedio)
+    maximos.append(maxi[K+1])
+    minimos.append(mini[K+1])
+    
+    
+    print(maximos.pop())
+    if poblacion[0].count(maxi[K+1]) >= PORCENTA:
+        PARAR = True
+
     for i in range(len(poblacion)):
         poblacion[i].pop()
         poblacion[i].pop()
+
+    #poda
+    print("Valor de pablacion: ",len(poblacion))
+    for i in range(0,(len(poblacion)-TMAX)):
+        print("Quitar: ",i)
+        poblacion.pop(i)
+
     T0 = len(poblacion)
+    print("T0: ",T0)
     parar +=1
 
+
+################ 
+
+
+plt.plot(minimos, marker='x', linestyle=':', color='b', label = "Minimos")
+plt.plot(promedios, marker='*', linestyle='-', color='g', label = "Promedios")
+plt.plot(maximos, marker='o', linestyle='--', color='r', label = "Maximos")
+plt.legend(loc="upper left")
+plt.show()
 
 
 
